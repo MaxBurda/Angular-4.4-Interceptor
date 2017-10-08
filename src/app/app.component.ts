17f2203623dc41f0ac9fff230beae95fabf648ba
Max <maxburda@gmail.com>
2017-10-09 00:00:27 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+
+interface UserResponse {
+  login: string,
+  bio: string,
+  company: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<UserResponse>('https://api.github.com/users/maxburda')
+      .subscribe(data => {
+        console.log(data.login);
+        console.log(data.bio);
+        console.log(data.company);
+      },
+        (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side Error occurred");
+        } else {
+          console.log("Server-side Error occurred");
+        }
+
+      }
+    );
+    const req =this.http.post('https://jsonplaceholder.typicode.com/posts', {
+      title: 'foo',
+      body: 'bar',
+      userUd: 1
+    })
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occurred");
+        }
+      )
+  }
 }
